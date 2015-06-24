c4dc15ca4908369b79498cf2e1cb9c92858412af
yoso <yoavyoso@gmail.com>
2015-06-24 23:54:06 +0300

    1. Add inappbrowser - doesn't work
    2. Add recipesService
    3. Add getRecipes from amazon - bug in parsing heb json

--- www/scripts/recipes/recipesController.js
+++ www/scripts/recipes/recipesController.js
@@ -1,16 +1,40 @@
 
-weissRecipesApp.controller('recipesController', ['$scope', '$stateParams', function($scope, $stateParams) {
+weissRecipesApp.controller('recipesController', ['$scope', '$stateParams','$http','$q','recipesService', function($scope, $stateParams, $http,$q, recipesService) {
 
   console.log($stateParams.categoryID);
   $scope.id = $stateParams.categoryID;
 
-  $scope.OpenUrl = function() {
-  navigator.app.loadUrl('https://s3.amazonaws.com/weissrecipes/Soups/soup1.jpg', { openExternal:true });
-   }
-  $scope.recipes=[
+  var model = {
+    recipes: []//recipesService.content
 
-    'מרק ירקות 1 '
-    ,'מרק בטטה'
-    ,'מרק עוף'];
+  };
+
+   getRecipes();
+  // getRecipes().then(function(recipes){
+  ////   model.recipes = recipes;
+  // });
+
+   function getRecipes(){
+    //   var defer = $q.defer();
+
+
+      //  $http.get({
+      //        url: 'https://s3.amazonaws.com/weissrecipes/Soups/soupslist.json',
+      //        dataType: 'json',
+      //        headers: {"Content-Type": "application/json;charset=utf-8" }
+      //  })
+       $http.get('https://s3.amazonaws.com/weissrecipes/Soups/soupslist.json')
+      .success(function(data){
+         model.recipes = data;
+    //          defer.resolve(data);
+       })
+       .error(function(err, status){
+           console.log(err, status);
+       });
+
+    //   return defer.promise;
+   }
+
+  $scope.model = model;
 
 }]);
---
+++ www/scripts/recipes/recipesService.js
@@ -0,0 +1,21 @@
+weissRecipesApp.service('recipesService', function($http,$q) {
+
+  this.getRecipes = function(){
+      var defer = $q.defer();
+
+      $http.get({
+            url: 'https://s3.amazonaws.com/weissrecipes/Soups/soupslist.json',
+            dataType: 'json',
+            headers: {"Content-Type": "application/json;charset=utf-8" }
+      })
+      .success(function(data){
+             defer.resolve(data);
+      })
+      .error(function(err, status){
+          console.log(err, status);
+      });
+
+      return defer.promise;
+  }
+
+});
